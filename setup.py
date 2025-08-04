cedf2f383d6a1c5a64872566f95d86ee622b8541
Rae Knowler <rae.knowler@liip.ch>
2025-08-04 15:35:17 +0200

    feat: Update Python requirements in setup.py

--- setup.py
+++ setup.py
@@ -33,13 +33,16 @@ setup(
         'Development Status :: 4 - Beta',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',  # noqa
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
-
+    python_requires=">=3.8",
     # What does your project relate to?
     keywords='''CKAN users''',
 
