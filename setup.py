8002709106e707981dc810ec7d71bf2be91fcaf3
Rae Knowler <rae.knowler@liip.ch>
2025-11-24 18:18:20 +0100

    feat: Update to pyproject.toml packaging
    See https://github.com/pypa/pip/issues/11457 and https://github.com/ckan/ckan/issues/8382.

--- setup.py
+++ setup.py
@@ -1,55 +1,6 @@
-from os import path
-
-from setuptools import find_packages, setup
-
-here = path.abspath(path.dirname(__file__))
-
+from setuptools import setup
 
 setup(
-    name="""ckanext-switzerland_users""",
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # http://packaging.python.org/en/latest/tutorial.html#version
-    version="0.0.1",
-    description="""User Management for opendata.swiss""",
-    # The project's main homepage.
-    url="https://github.com/ogdch/ckanext-switzerland_users",
-    # Author details
-    author="""Liip AG""",
-    author_email="""ogdch@liip.ch""",
-    # Choose your license
-    license="AGPL",
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        # How mature is this project? Common values are
-        # 3 - Alpha
-        # 4 - Beta
-        # 5 - Production/Stable
-        "Development Status :: 4 - Beta",
-        # Pick your license as you wish (should match "license" above)
-        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
-    python_requires=">=3.8",
-    # What does your project relate to?
-    keywords="""CKAN users""",
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    entry_points="""
-    [ckan.plugins]
-        ogdch_users=ckanext.switzerland_users.plugin:OgdchUsersPlugin
-    [babel.extractors]
-        ckan = ckan.lib.extract:extract_ckan
-    """,
     message_extractors={
         "ckanext": [
             ("**.py", "python", None),
